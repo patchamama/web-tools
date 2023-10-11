d71c0a394b92048383c2b6b122c5e351df306aed
A. Urquiola <patchamama.go@gmail.com>
2023-10-11 22:07:01 +0200

    new proposed feature

--- scripts/github-pullrequest.sh
+++ scripts/github-pullrequest.sh
@@ -6,8 +6,27 @@ collaboration=""
 if [ -f "collaboration.txt" ]; then
     collaboration=$(head -n 1 collaboration.txt)
 fi
+echo Collaboration defines: $collaboration
 
-echo $collaboration
+echo "Do you want to add a collaboration name? (y/n)"
+read answer
+if [ "$answer" == "y" ]
+then
+    echo "Enter the collaboration name:"
+    read name
+    echo "Enter the collaboration github name:"
+    read githubname
+    collaboration="$collaboration Co-authored-by: $name <$githubname@users.noreply.github.com> "
+
+    echo $collaboration > collaboration.txt
+    # git commit -m "README updated Co-authored-by: Armando Urquiola Cabrera <patchamama@users.noreply.github.com> Co-authored-by: Alberto Cabrera <albecabrera@users.noreply.github.com>"
+    echo Collaboration added: $collaboration
+
+    bash $0
+    exit 0
+fi
+
+# read -p "Press enter to continue"
 
 # Clone the repository
 #git clone <repository_url>
@@ -33,19 +52,19 @@ git add .
 # Commit the changes
 git commit -m "new proposed feature $collaboration"
 
-# Push the branch to GitHub
-git push origin new-proposed-feature
-
-# Create a pull request on GitHub
-echo Go to the repository page on GitHub
-echo Click on the "Pull Requests" tab
-echo Click on the "New Pull Request" button
-echo Select the branch you just pushed
-echo  Click on the "Create Pull Request" button
-open -a "Google Chrome" https://github.com/$repousername/$reponame
-
 if ! [ "$newbranchname" == "" ]
 then
+
+    # Push the branch to GitHub
+    git push origin $newbranchname
+
+    # Create a pull request on GitHub
+    echo Go to the repository page on GitHub
+    echo Click on the "Pull Requests" tab
+    echo Click on the "New Pull Request" button
+    echo Select the branch you just pushed
+    echo  Click on the "Create Pull Request" button
+    open -a "Google Chrome" https://github.com/$repousername/$reponame
 
     echo "Do you want to merge with the main branch the pull request? (y/n)"
     read answer
@@ -63,6 +82,9 @@ then
         git branch -d new-proposed-feature
         # git push origin --delete new-proposed-feature
     fi
+else
+    git push
+    # git push origin main
 fi
 
 git branch
